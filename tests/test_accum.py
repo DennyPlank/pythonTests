6530fa693610209f719e1dcce5e909f49a68c54b
Dennis Plank <DennyMouzon@gmail.com>
2022-09-02 10:27:51 -0700

    acculamtor test

---
+++ tests/test_accum.py
@@ -0,0 +1,28 @@
+from operator import attrgetter
+import pytest
+from stuff.accum import Accumulator
+
+def test_accum_init():
+    accum = Accumulator()
+    assert accum.count == 0
+
+def test_accum_add_one():
+    accum = Accumulator()
+    accum.add()
+    assert accum.count == 1
+
+def test_accum_init():
+    accum = Accumulator()
+    accum.add(3)
+    assert accum.count == 3
+
+def test_accum_add_twice():
+    accum = Accumulator()
+    accum.add()
+    accum.add()
+    assert accum.count == 2
+
+def test_accumulator_cannot_set_count_directly():
+    accum = Accumulator()
+    with pytest.raises(AttributeError, match=r"can't set attribute") as e:
+        accum.count = 10
