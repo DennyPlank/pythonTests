eeb586b6a42cd0008edb33d42ddda4f915ea5ec8
Dennis Plank <DennyMouzon@gmail.com>
2022-09-02 10:34:14 -0700

    used a pytest fixture

--- tests/test_accum.py
+++ tests/test_accum.py
@@ -2,27 +2,27 @@ from operator import attrgetter
 import pytest
 from stuff.accum import Accumulator
 
-def test_accum_init():
-    accum = Accumulator()
+# Fixtures should always return a value
+@pytest.fixture 
+def accum():
+    return Accumulator()
+
+def test_accum_init(accum):
     assert accum.count == 0
 
-def test_accum_add_one():
-    accum = Accumulator()
+def test_accum_add_one(accum):
     accum.add()
     assert accum.count == 1
 
-def test_accum_init():
-    accum = Accumulator()
+def test_accum_init(accum):
     accum.add(3)
     assert accum.count == 3
 
-def test_accum_add_twice():
-    accum = Accumulator()
+def test_accum_add_twice(accum):
     accum.add()
     accum.add()
     assert accum.count == 2
 
-def test_accumulator_cannot_set_count_directly():
-    accum = Accumulator()
+def test_accumulator_cannot_set_count_directly(accum):
     with pytest.raises(AttributeError, match=r"can't set attribute") as e:
         accum.count = 10
